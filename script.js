e4cd73f1671333b3978e39726b7f84ff876f8619
Jhonatan Santiago <jhonssantiago@gmail.com>
2021-04-07 10:45:42 -0300

    tela inicial

--- script.js
+++ script.js
@@ -1,65 +1,63 @@
-const cards = document.querySelectorAll('.card');
-let hasFlipperCard = false;
-let firstCard, secondCard;
-let lockBoard = false;
+function inicio() {
+    $("#start").hide();
+    $(".card").show();
 
-function flipCard() {
-    if (lockBoard) return; //travar tabuleiro
-    if (this === firstCard) return; //duplo clique na mesma carta
+    const cards = document.querySelectorAll('.card');
+    let hasFlipperCard = false; //verifica se tem cartao selecionado
+    let firstCard, secondCard;
+    let lockBoard = false;
 
-    this.classList.add('flip');
-    if(!hasFlipperCard) {
-        hasFlipperCard = true;
-        firstCard = this;
-        return;
+    function flipCard() { //revela imagem da carta
+        if (lockBoard) return; //travar tabuleiro
+        if (this === firstCard) return; //duplo clique na mesma carta
+        this.classList.add('flip');
+        if(!hasFlipperCard) {
+            hasFlipperCard = true;
+            firstCard = this;
+            return;
+        }
+        secondCard = this;
+        hasFlipperCard = false;
+        checkForMath(); 
     }
 
-    secondCard = this;
-    hasFlipperCard = false;
-    checkForMath();
-}
-
-
-function checkForMath() {
-    if(firstCard.dataset.card === secondCard.dataset.card){
-        disableCards();
-        return;
+    function checkForMath() { //verifica se cartas sao identicas
+        if(firstCard.dataset.card === secondCard.dataset.card){
+            disableCards(); // chama metodo desativar cartas
+            return;
+        }
+        unflipCards(); //chama metodo mostra fundo das cartas
     }
 
-    unflipCards();
-}
-
-function disableCards(){
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-    resetBoard();
-}
-
-function unflipCards() {
-    lockBoard = true;
-    setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
+    function disableCards(){ //desativar cartas
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
         resetBoard();
-    }, 1500);
+    }
+
+    function unflipCards() { //mostra fundo da carta
+        lockBoard = true; //trava tabuleiro
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            resetBoard();
+        }, 1500);
+    } 
+
+    function resetBoard() { //renicia tabuleiro
+        [hasFlipperCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    (function shuffle() { //embaralhar cartas
+        cards.forEach((card) => {
+            let randomPosition = Math.floor(Math.random() * 12);
+            card.style.order = randomPosition;
+        })
+    })();
+
+    cards.forEach((card) => { //define carta clicada
+        card.addEventListener('click', flipCard)
+    });
+
 }
-
-function resetBoard() {
-    [hasFlipperCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-    cards.forEach((card) => {
-        let randomPosition = Math.floor(Math.random() * 12);
-        card.style.order = randomPosition;
-    })
-})();
-
-
-cards.forEach((card) => {
-    card.addEventListener('click', flipCard)
-});
-
-
-
